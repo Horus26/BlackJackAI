ca1546aedcf0b69d56c05725fef7d4e95776816c
Horus26 <51298668+Horus26@users.noreply.github.com>
2022-03-29 15:26:01 +0200

    Card and Carddeck
    Card and Carddeck classes created

---
+++ Carddeck.py
@@ -0,0 +1,32 @@
+from Card import Card
+
+COLOR_SPADE = u'\u2660'
+COLOR_CLUB = u'\u2663'
+COLOR_HEART =  u'\u2665'
+COLOR_DIAMOND =  u'\u2666'
+
+COLORS = [COLOR_SPADE, COLOR_CLUB, COLOR_HEART, COLOR_DIAMOND]
+
+
+class Carddeck :
+    def __init__(self):
+        self.deck = []
+        for color in COLORS:
+
+            # add number cards
+            for value in range(2, 11):
+                self.deck.append(Card(color + str(value), value, color))
+            # add image cards
+            for i in ["J", "Q", "K"]:
+                self.deck.append(Card(color + i, 10, color))
+            # add ace 
+            # TODO: WATCH OUT HANDLING ACE VALUE 11
+            self.deck.append(Card(color + "A", 1, color))
+
+    def printDeck(self):
+        for card in self.deck:
+            print(card)
+
+if __name__ == "__main__":
+    carddeck = Carddeck()
+    carddeck.printDeck()
